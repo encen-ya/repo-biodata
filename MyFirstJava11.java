030a471294443de15863543a1a1dbacff655772d
encen-ya <echenwijono@gmail.com>
2024-09-11 17:59:22 +0700

    commit pertama

---
+++ MyFirstJava11.java
@@ -0,0 +1,20 @@
+/**
+ * MyFirstJava11
+ */
+public class MyFirstJava11 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");    
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("Nama    : Kreszen Vallentino Arjuna Wijono");    
+        System.out.println("NIM     : 244107020206");    
+        System.out.println("Kelas   : 1G");    
+        System.out.println("No Absen: 11");    
+        System.out.println("Prodi   : Teknik Informatika");    
+        System.out.println("Alamat  : Jln Kembang Kertas no 5C ");    
+        System.out.println("No HP   : 085808266894");    
+        System.out.println("Hobi    : Melihat Alam,olahraga");    
+    }
+}
